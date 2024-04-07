2beee1d42f7207ef46f7b47ce6966b7ffd12e981
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-04-07 10:20:37 -0300

    feat: adding submit login button

---
+++ src/components/Buttons/SubmitLoginButton/SubmitLoginButton.tsx
@@ -0,0 +1,10 @@
+import Button from 'react-bootstrap/Button'
+import './SubmitLoginButton.css'
+function SubmitLoginButton() {
+  return (
+    <Button id = "submission-button" variant="primary" type="submit"> Entrar </Button>
+      
+  )
+}
+
+export default SubmitLoginButton
