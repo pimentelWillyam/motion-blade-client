4e40403c8ec9173611055942395d5ab47f19f3ac
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-04-11 10:07:20 -0300

    feat: adding back to login page button

---
+++ src/components/Buttons/BackToLoginPageButton/BackToLoginPageButton.tsx
@@ -0,0 +1,11 @@
+import Button from 'react-bootstrap/Button'
+import './BackToLoginPageButton.css'
+
+function BackToLoginPageButton() {
+  return (
+    <Button href='/' id = "back-to-login-page-button" variant="warning" type="submit"> Voltar </Button>
+      
+  )
+}
+
+export default BackToLoginPageButton
