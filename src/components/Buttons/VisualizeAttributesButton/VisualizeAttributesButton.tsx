d85370d52e1314216b94986a20547f7e423f1a7f
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-04-11 16:20:23 -0300

    feat: adding visualize attributes button

---
+++ src/components/Buttons/VisualizeAttributesButton/VisualizeAttributesButton.tsx
@@ -0,0 +1,11 @@
+import Button from 'react-bootstrap/Button'
+import './VisualizeAttributesButton.css'
+
+function VisualizeAttributesButton() {
+  return (
+    <Button href='/' id = "back-to-login-page-button" variant="primary" type="submit"> Atributos </Button>
+      
+  )
+}
+
+export default VisualizeAttributesButton
