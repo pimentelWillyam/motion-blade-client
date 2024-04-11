02b67b3726a28e6f84ef4bfab1dff4c363ffe9a9
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-04-11 10:09:08 -0300

    feat: adding submit servant registry button

---
+++ src/components/Buttons/SubmitServantRegistryButton/SubmitServantRegistryButton.tsx
@@ -0,0 +1,10 @@
+import Button from 'react-bootstrap/Button'
+import './SubmitServantRegistryButton.css'
+function SubmitServantRegistryButton() {
+  return (
+    <Button id = "submit-servant-registry-button" variant="success" type="submit"> Cadastrar Servo </Button>
+      
+  )
+}
+
+export default SubmitServantRegistryButton
