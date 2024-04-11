48ef25ee635af98ed5be7e9b0cf58cdb18cd9ac4
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-04-11 10:11:26 -0300

    feat: adding all routes

--- src/router.tsx
+++ src/router.tsx
@@ -1,8 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import LoginPage from "./pages/LoginPage/LoginPage";
+import HomePage from "./pages/HomePage/HomePage";
+import { RegisterNewUserPage } from "./pages/RegisterNewUserPage/RegisterNewUserPage";
+import { RegisterNewServantPage } from "./pages/RegisterServantPage/RegisterNewServantPage";
+import { RegisterNewBattlePage } from "./pages/RegisterBattlePage/RegisterBattlePage";
 
 const router = createBrowserRouter([
-  {path: '/', element: <LoginPage/>}
+  {path: '/', element: <LoginPage/>},
+  {path: '/cadastrar-usuario', element: <RegisterNewUserPage/>},
+  {path: '/home', element: <HomePage/>},
+  {path: '/cadastrar-servo', element: <RegisterNewServantPage/>},
+  {path: '/meus-servos', element: <HomePage/>},
+  {path: '/cadastrar-batalha', element: <RegisterNewBattlePage/>},
+  {path: '/batalhas', element: <HomePage/>},
+
+
+
+
 ])
 
 export { router }
