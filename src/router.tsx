ed7de480562c04fbfba8ff3599a50476d40e1719
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-04-11 16:21:26 -0300

    feat: addin g my servants page to the router

--- src/router.tsx
+++ src/router.tsx
@@ -4,13 +4,14 @@ import HomePage from "./pages/HomePage/HomePage";
 import { RegisterNewUserPage } from "./pages/RegisterNewUserPage/RegisterNewUserPage";
 import { RegisterNewServantPage } from "./pages/RegisterServantPage/RegisterNewServantPage";
 import { RegisterNewBattlePage } from "./pages/RegisterBattlePage/RegisterBattlePage";
+import MyServantsPage from "./pages/MyServantsPage/MyServantsPage";
 
 const router = createBrowserRouter([
   {path: '/', element: <LoginPage/>},
   {path: '/cadastrar-usuario', element: <RegisterNewUserPage/>},
   {path: '/home', element: <HomePage/>},
   {path: '/cadastrar-servo', element: <RegisterNewServantPage/>},
-  {path: '/meus-servos', element: <HomePage/>},
+  {path: '/meus-servos', element: <MyServantsPage/>},
   {path: '/cadastrar-batalha', element: <RegisterNewBattlePage/>},
   {path: '/batalhas', element: <HomePage/>},
 
