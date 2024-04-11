bf13a2d752876adbdcf276965e9d014b89120a6a
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-04-11 10:09:58 -0300

    feat: adding register new user form

---
+++ src/components/Forms/RegisterNewUserForm/RegisterNewUserForm.tsx
@@ -0,0 +1,31 @@
+import './RegisterNewUserForm.css'
+
+import Form from 'react-bootstrap/Form'
+import { handleRegisterSubmission } from '../../../handlers/handleRegisterSubmission';
+import BackToLoginPageButton from '../../Buttons/BackToLoginPageButton/BackToLoginPageButton';
+import SubmitRegisterButton from '../../Buttons/SubmitRegisterButton/SubmitRegisterButton';
+
+function RegisterNewUserForm() {
+  return (
+    <div id="register-new-user-form">
+      <Form onSubmit={void handleRegisterSubmission()}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Control type="email" placeholder="Insira seu usuário" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Control type="email" placeholder="Insira seu email" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Control type="password" placeholder="Insira sua senha" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Control type="password" placeholder="Repita a sua senha" />
+        </Form.Group>
+       <BackToLoginPageButton /> <SubmitRegisterButton />
+    </Form>
+    </div>
+    
+  )
+}
+
+export default RegisterNewUserForm;
