6c5f2a3cd199241a614eb706fe10991938fbcb4f
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-04-11 16:20:39 -0300

    feat: adding visualize inventory button

---
+++ src/components/Buttons/VisualizeInventoryButton/VisualizeInventoryButton.tsx
@@ -0,0 +1,11 @@
+import Button from 'react-bootstrap/Button'
+import './VisualizeInventoryButton.css'
+
+function VisualizeAttributesButton() {
+  return (
+    <Button href='/' id = "back-to-login-page-button" variant="warning" type="submit"> Inventário </Button>
+      
+  )
+}
+
+export default VisualizeAttributesButton
