f3cedbd73f5af221a0a1a56ffff7a95cb2fc2359
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-04-11 10:08:54 -0300

    feat: adding submit register button

---
+++ src/components/Buttons/SubmitRegisterButton/SubmitRegisterButton.tsx
@@ -0,0 +1,10 @@
+import Button from 'react-bootstrap/Button'
+import './SubmitRegisterButton.css'
+function SubmitRegisterButton() {
+  return (
+    <Button id = "submission-button" variant="success" type="submit"> Cadastrar </Button>
+      
+  )
+}
+
+export default SubmitRegisterButton
