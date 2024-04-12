bf8219c6a37b31d951727b39ffd4e133852441ce
Willyam Pimentel <willyampimenteldev@gmail.com>
2024-04-12 13:43:37 -0300

    feat: adding enter battle button

---
+++ src/components/Buttons/EnterBattleButton/EnterBattleButton.tsx
@@ -0,0 +1,10 @@
+import Button from 'react-bootstrap/Button'
+import './EnterBattleButton.css'
+function EnterBattleButton() {
+  return (
+    <Button id = "enter-battle-button" variant="success" type="submit"> Junta-se a batalha </Button>
+      
+  )
+}
+
+export default EnterBattleButton
