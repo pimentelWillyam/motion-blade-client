40b281d5768d8a7ce5cad8f61b0d7b9f308fb3b4
Willyam Pimentel <willyampimenteldev@gmail.com>
2025-03-01 12:06:36 -0300

    feat: adding register new user form

--- src/components/Forms/RegisterNewUserForm/RegisterNewUserForm.tsx
+++ src/components/Forms/RegisterNewUserForm/RegisterNewUserForm.tsx
@@ -1,30 +1,62 @@
 import './RegisterNewUserForm.css'
 
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form'
 import { handleRegisterSubmission } from '../../../handlers/handleRegisterSubmission';
 import BackToLoginPageButton from '../../Buttons/BackToLoginPageButton/BackToLoginPageButton';
 import SubmitRegisterButton from '../../Buttons/SubmitRegisterButton/SubmitRegisterButton';
 
-function RegisterNewUserForm() {
+function RegisterNewUserForm(props: {setIsLoadingModal: React.Dispatch<React.SetStateAction<boolean>>, setShowUserRegisteredToast: React.Dispatch<React.SetStateAction<boolean>>, setShowUserRegistryFailToast: React.Dispatch<React.SetStateAction<boolean>>, }) {
+  const [userName, setUserName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [repeatedPassword, setRepeatedPassword] = useState('');
+
+  const clearUserForm = () => {
+    setUserName('');
+    setEmail('');
+    setPassword('');
+    setRepeatedPassword('');
+  }
+
   return (
     <div id="register-new-user-form">
-      <Form onSubmit={void handleRegisterSubmission()}>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Control type="email" placeholder="Insira seu usuário" />
+      <Form onSubmit={event => void handleRegisterSubmission(event, userName, email, password, repeatedPassword, clearUserForm, props.setIsLoadingModal, props.setShowUserRegisteredToast, props.setShowUserRegistryFailToast)}>
+        <Form.Group className="mb-3" controlId="formBasicUserName">
+          <Form.Control 
+            type="text" 
+            placeholder="Insira seu usuário" 
+            value={userName}
+            onChange={e => setUserName(e.target.value)}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Control type="email" placeholder="Insira seu email" />
+          <Form.Control 
+            type="email" 
+            placeholder="Insira seu email" 
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Control type="password" placeholder="Insira sua senha" />
+          <Form.Control 
+            type="password" 
+            placeholder="Insira sua senha" 
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Control type="password" placeholder="Repita a sua senha" />
+        <Form.Group className="mb-3" controlId="formBasicRepeatedPassword">
+          <Form.Control 
+            type="password" 
+            placeholder="Repita a sua senha" 
+            value={repeatedPassword}
+            onChange={e => setRepeatedPassword(e.target.value)}
+          />
         </Form.Group>
        <BackToLoginPageButton /> <SubmitRegisterButton />
     </Form>
     </div>
-    
   )
 }
 
