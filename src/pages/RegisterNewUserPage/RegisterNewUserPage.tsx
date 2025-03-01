353fc69385bd9f28fdee1e37df8b47bdd017efa5
Willyam Pimentel <willyampimenteldev@gmail.com>
2025-03-01 12:07:49 -0300

    refactor: adding modals and toasts to user registry page

--- src/pages/RegisterNewUserPage/RegisterNewUserPage.tsx
+++ src/pages/RegisterNewUserPage/RegisterNewUserPage.tsx
@@ -3,11 +3,23 @@ import './RegisterNewUserPage.css'
 
 import RegisterNewUserForm from '../../components/Forms/RegisterNewUserForm/RegisterNewUserForm';
 import Container from 'react-bootstrap/Container'
+import LoadingModal from '../../components/Modals/LoadingModal/LoadingModal';
+import { useState } from 'react';
+import UserRegisteredToast from '../../components/Toasts/UserRegisteredToast/UserRegisteredToast';
+import UserRegistryFailToast from '../../components/Toasts/UserRegistryFailToast/UserRegistryFailToast';
+import { useNavigate } from 'react-router-dom';
 
 const RegisterNewUserPage = () => {
+  const navigate = useNavigate()
+  const [isLoading, setIsLoading] = useState(false);
+  const [showUserRegisteredToast, setShowUserRegisteredToast] = useState(false);
+  const [showUserRegistryFailToast, setShowUserRegistryFailToast] = useState(false);
   return (
     <Container id = 'register-user-page'>
-      <RegisterNewUserForm />
+      <LoadingModal show={isLoading}/>
+      <UserRegisteredToast show={showUserRegisteredToast} onClose={() => navigate('/')} />
+      <UserRegistryFailToast show={showUserRegistryFailToast} onClose={() => navigate('/')} />
+      <RegisterNewUserForm setIsLoadingModal={setIsLoading} setShowUserRegisteredToast={setShowUserRegisteredToast} setShowUserRegistryFailToast={setShowUserRegistryFailToast} />
     </Container>
 
         
