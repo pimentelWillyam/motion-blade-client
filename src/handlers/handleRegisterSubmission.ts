71cedb0087a38d4ac6b6819f021fe1bff839c4d8
Willyam Pimentel <willyampimenteldev@gmail.com>
2025-03-01 12:07:30 -0300

    feat: adding handler for user registry submission

--- src/handlers/handleRegisterSubmission.ts
+++ src/handlers/handleRegisterSubmission.ts
@@ -1,5 +1,29 @@
-const handleRegisterSubmission = () => {
-  console.log('login handled')
+import axios from 'axios';
+
+const handleRegisterSubmission = async (event: React.FormEvent<HTMLFormElement>, userName: string, email: string, password: string, repeatedPassword: string, clearUserForm: () => void, setIsLoadingModal: React.Dispatch<React.SetStateAction<boolean>>, setShowUserRegisteredToast: React.Dispatch<React.SetStateAction<boolean>>, setShowUserRegistryFailToast: React.Dispatch<React.SetStateAction<boolean>>) => {
+  event.preventDefault();
+  const data = {
+    login: userName,
+    password: password,
+    email: email,
+  };
+  try {
+    setIsLoadingModal(true)
+    clearUserForm()
+    const response = await axios.post('http://localhost:4000/api/user', data);
+    if (response.status === 200) {
+      setIsLoadingModal(false)
+      setShowUserRegisteredToast(true)
+    }
+    else {
+      setIsLoadingModal(false)
+      setShowUserRegistryFailToast(true)
+    }
+  }catch (error) {
+    console.error('Error during registration:', error);
+    setIsLoadingModal(false)
+    setShowUserRegistryFailToast(true)
+  }
 }
 
-export {handleRegisterSubmission}
+export { handleRegisterSubmission };
