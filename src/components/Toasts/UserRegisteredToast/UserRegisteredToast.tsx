80c3a50a3151bef33abe273f43c32604ca105c84
Willyam Pimentel <willyampimenteldev@gmail.com>
2025-03-03 16:11:17 -0300

    feat: adding user registered toast

--- src/components/Toasts/UserRegisteredToast/UserRegisteredToast.tsx
+++ src/components/Toasts/UserRegisteredToast/UserRegisteredToast.tsx
@@ -1,13 +1,22 @@
 import { Toast } from 'react-bootstrap';
+import { CheckCircle, X } from 'lucide-react'; // Ícones
 import './UserRegisteredToast.css';
 
 function UserRegisteredToast({ show, onClose }: { show: boolean, onClose: () => void }) {
   return (
-    <Toast id='toast' show={show} onClose={onClose} delay={3000} autohide>
-      <Toast.Header className='toast-header-success'>
-        <strong className="me-auto">Sucesso</strong>
-      </Toast.Header>
-      <Toast.Body className='toast-body'>Usuário registrado com sucesso!</Toast.Body>
+    <Toast show={show} onClose={onClose} delay={3000} autohide className="custom-toast-success">
+      <div className="toast-content">
+        <div className="toast-icon">
+          <CheckCircle size={20} color="#2d6a4f" />
+        </div>
+        <div className="success-toast-text">
+          <strong>Sucesso</strong>
+          <p>Usuário registrado com sucesso.</p>
+        </div>
+        <button className="toast-close" onClick={onClose}>
+          <X size={18} />
+        </button>
+      </div>
     </Toast>
   );
 }
