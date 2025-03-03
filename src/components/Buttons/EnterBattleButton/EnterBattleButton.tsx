192a6364acc31fab679382e68957d18435de1a98
Willyam Pimentel <willyampimenteldev@gmail.com>
2025-03-03 16:07:16 -0300

    fix: fixing enter battle button

--- src/components/Buttons/EnterBattleButton/EnterBattleButton.tsx
+++ src/components/Buttons/EnterBattleButton/EnterBattleButton.tsx
@@ -1,8 +1,10 @@
 import Button from 'react-bootstrap/Button'
 import './EnterBattleButton.css'
-function EnterBattleButton() {
+import { useNavigate } from 'react-router-dom'
+function EnterBattleButton(props: {nomeBatalha: string}) {
+  const navigate = useNavigate()
   return (
-    <Button id = "enter-battle-button" variant="success" type="submit"> Junta-se a batalha </Button>
+    <Button id = "enter-battle-button" variant="success" type="button" onClick={() => navigate(`/batalha/${props.nomeBatalha}`)}> Junta-se a batalha </Button>
       
   )
 }
