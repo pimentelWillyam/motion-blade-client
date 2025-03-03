96808861a7a24bacd64cf97280aa8f1522c9b0d6
Willyam Pimentel <willyampimenteldev@gmail.com>
2025-03-03 16:10:32 -0300

    feat: adding servants table

--- src/components/Tables/MyServantsTable/MyServantsTable.tsx
+++ src/components/Tables/MyServantsTable/MyServantsTable.tsx
@@ -1,28 +1,61 @@
+import { useEffect, useState } from 'react';
 import Table from 'react-bootstrap/Table';
 import VisualizeAttributesButton from '../../Buttons/VisualizeAttributesButton/VisualizeAttributesButton';
 import VisualizeInventoryButton from '../../Buttons/VisualizeInventoryButton/VisualizeInventoryButton';
+import DatabaseServant from '../../../models/DatabaseServant';
+import axios from 'axios';
+import DeleteServantButton from '../../Buttons/DeleteServantButton/DeleteServantButton';
+import { handleLoginSubmission } from '../../../handlers/handleLoginSubmission';
+import { handleServantDeletion } from '../../../handlers/handleServantDeletion';
 
 function MyServantsTable() {
+  const [servants, setServants] = useState<DatabaseServant[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchServants = async () => {
+      try {
+        const response = await axios.get(`http://localhost:4000/api/servants/${localStorage.getItem('login')}`); // Substitua pela URL real da API
+        if (response.status !== 200) {
+          throw new Error('Erro ao buscar os servos.');
+
+        }
+        setServants(response.data);
+      } catch (err) {
+        setError((err as Error).message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchServants();
+  }, []);
+
+  if (loading) return <p>Carregando...</p>;
+  if (error) return <p>Erro: {error}</p>;
+
   return (
     <Table striped bordered hover>
       <thead>
         <tr>
-          <th>#</th>
           <th>Nome</th>
-          <th>Nível</th>
           <th></th>
           <th></th>
+          <th></th>
+
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>1</td>
-          <td>Fulano</td>
-          <td>10</td>
-          <td><VisualizeAttributesButton /></td>
-          <td><VisualizeInventoryButton /></td>
+        {servants.map((servant) => (
+          <tr key={servant.id}>
+            <td>{servant.name}</td>
+            <td><VisualizeAttributesButton /></td>
+            <td><VisualizeInventoryButton /></td>
+            <td><DeleteServantButton onClick={() => handleServantDeletion(servant.name, props.setIsLoadingModal, props.setSuccessToast, setFailureToast)} /></td>
 
-        </tr>
+          </tr>
+        ))}
       </tbody>
     </Table>
   );
