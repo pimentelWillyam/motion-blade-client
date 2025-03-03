f9969e710dd8a6ec6a07e6051e1072f9d3887fbc
Willyam Pimentel <willyampimenteldev@gmail.com>
2025-03-03 16:15:18 -0300

    feat: adding battles pages

--- src/pages/BattlesPage/BattlesPage.tsx
+++ src/pages/BattlesPage/BattlesPage.tsx
@@ -5,8 +5,22 @@ import Container from 'react-bootstrap/Container'
 import NavigationBar from '../../components/NavigationBars/NavigationBar/NavigationBar';
 import Row from 'react-bootstrap/Row'
 import BattlesTable from '../../components/Tables/BattlesTable/BattlesTable';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const BattlesPage = () => {
+  
+  const navigate = useNavigate();
+
+  useEffect(() => {
+      const login = localStorage.getItem("login")
+      const token = localStorage.getItem("token")
+  
+      if (!login && !token ) {
+        navigate("/");
+      }
+    }, []);
+
   return (
     <Container id = "my-servants-page">
       <Row id = "my-servants-page-first-row">
