c8fd5c050397dccfd1f44acd542d085b8e88ba86
Willyam Pimentel <willyampimenteldev@gmail.com>
2025-03-03 16:05:37 -0300

    refactor: fixing back to home page button

--- src/components/Buttons/BackToHomePageButton/BackToHomePageButton.tsx
+++ src/components/Buttons/BackToHomePageButton/BackToHomePageButton.tsx
@@ -1,9 +1,11 @@
 import Button from 'react-bootstrap/Button'
 import './BackToHomePageButton.css'
+import { useNavigate } from 'react-router-dom'
 
 function BackToHomePageButton() {
+  const navigate = useNavigate()
   return (
-    <Button href='/home' id = "back-to-home-page-button" variant="warning" type="submit"> Voltar </Button>
+    <Button onClick={() => navigate('/home')} id = "back-to-home-page-button" variant="warning" type="submit"> Voltar </Button>
       
   )
 }
