d875772fcbd752d2cb2492c610debbeba314a149
Willyam Pimentel <willyampimenteldev@gmail.com>
2025-03-03 16:16:32 -0300

    feat: adding register new user page

--- src/pages/RegisterNewUserPage/RegisterNewUserPage.tsx
+++ src/pages/RegisterNewUserPage/RegisterNewUserPage.tsx
@@ -4,21 +4,32 @@ import './RegisterNewUserPage.css'
 import RegisterNewUserForm from '../../components/Forms/RegisterNewUserForm/RegisterNewUserForm';
 import Container from 'react-bootstrap/Container'
 import LoadingModal from '../../components/Modals/LoadingModal/LoadingModal';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import UserRegisteredToast from '../../components/Toasts/UserRegisteredToast/UserRegisteredToast';
 import UserRegistryFailToast from '../../components/Toasts/UserRegistryFailToast/UserRegistryFailToast';
 import { useNavigate } from 'react-router-dom';
 
 const RegisterNewUserPage = () => {
-  const navigate = useNavigate()
+
+  const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
   const [showUserRegisteredToast, setShowUserRegisteredToast] = useState(false);
   const [showUserRegistryFailToast, setShowUserRegistryFailToast] = useState(false);
+  
+  useEffect(() => {
+      const login = localStorage.getItem("login")
+      const token = localStorage.getItem("token")
+  
+      if (!login && !token ) {
+        navigate("/");
+      }
+    }, []);
+
   return (
     <Container id = 'register-user-page'>
       <LoadingModal show={isLoading}/>
       <UserRegisteredToast show={showUserRegisteredToast} onClose={() => navigate('/')} />
-      <UserRegistryFailToast show={showUserRegistryFailToast} onClose={() => navigate('/')} />
+      <UserRegistryFailToast show={showUserRegistryFailToast} onClose={() => {setShowUserRegistryFailToast(false)} } />
       <RegisterNewUserForm setIsLoadingModal={setIsLoading} setShowUserRegisteredToast={setShowUserRegisteredToast} setShowUserRegistryFailToast={setShowUserRegistryFailToast} />
     </Container>
 
