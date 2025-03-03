4288678578fc87703379ce47843454a95f9c3d98
Willyam Pimentel <willyampimenteldev@gmail.com>
2025-03-03 16:15:42 -0300

    feat: adding my servants page

--- src/pages/MyServantsPage/MyServantsPage.tsx
+++ src/pages/MyServantsPage/MyServantsPage.tsx
@@ -5,8 +5,20 @@ import Container from 'react-bootstrap/Container'
 import NavigationBar from '../../components/NavigationBars/NavigationBar/NavigationBar';
 import Row from 'react-bootstrap/Row'
 import MyServantsTable from '../../components/Tables/MyServantsTable/MyServantsTable';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const MyServantsPage = () => {
+  const navigate = useNavigate();
+
+    useEffect(() => {
+        const login = localStorage.getItem("login")
+        const token = localStorage.getItem("token")
+
+        if (!login && !token ) {
+          navigate("/");
+        }
+      }, []);
   return (
     <Container id = "my-servants-page">
       <Row id = "my-servants-page-first-row">
@@ -16,8 +28,8 @@ const MyServantsPage = () => {
         <MyServantsTable />
       </Row>
     </Container>
-        
-      
+
+
   )
 }
 
