6ad7448dfbb45b55769e8ceb2b120bab852f1ea7
Willyam Pimentel <willyampimenteldev@gmail.com>
2025-03-03 16:11:27 -0300

    feat: adding user registry fail toast

--- src/components/Toasts/UserRegistryFailToast/UserRegistryFailToast.tsx
+++ src/components/Toasts/UserRegistryFailToast/UserRegistryFailToast.tsx
@@ -1,13 +1,22 @@
 import { Toast } from 'react-bootstrap';
+import { XCircle, X } from 'lucide-react'; // Ícones
 import './UserRegistryFailToast.css';
 
 function UserRegistryFailToast({ show, onClose }: { show: boolean, onClose: () => void }) {
   return (
-    <Toast show={show} onClose={onClose} delay={3000} autohide>
-      <Toast.Header className="toast-header-fail">
-        <strong className="me-auto">Erro</strong>
-      </Toast.Header>
-      <Toast.Body>Falha ao registrar o usuário!</Toast.Body>
+    <Toast show={show} onClose={onClose} delay={3000} autohide className="custom-toast-error">
+      <div className="toast-content">
+        <div className="toast-icon">
+          <XCircle size={20} color="#842029" />
+        </div>
+        <div className="fail-toast-text">
+          <strong>Erro</strong>
+          <p>Falha ao registrar o usuário.</p>
+        </div>
+        <button className="toast-close" onClick={onClose}>
+          <X size={18} />
+        </button>
+      </div>
     </Toast>
   );
 }
