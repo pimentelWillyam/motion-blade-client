244f1dcb7e82c963734bfd9388fa2b8d094e925b
Willyam Pimentel <willyampimenteldev@gmail.com>
2025-03-03 16:05:52 -0300

    feat: adding back to login page button

---
+++ src/components/Buttons/BackToLoginPageButton copy/BackToLoginPageButton.tsx
@@ -0,0 +1,13 @@
+import Button from 'react-bootstrap/Button'
+import './BackToLoginPageButton.css'
+import { useNavigate } from 'react-router-dom'
+
+function BackToLoginPageButton() {
+    const navigate = useNavigate()
+  return (
+    <Button onClick={() => navigate('/')}  variant="warning" type="submit"> Voltar </Button>
+      
+  )
+}
+
+export default BackToLoginPageButton
