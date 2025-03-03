88dcf8a3a1de5e2c4af8936a78ac8e0af0ca87c1
Willyam Pimentel <willyampimenteldev@gmail.com>
2025-03-03 16:09:04 -0300

    fix: fixing login form

--- src/components/Forms/LoginForm/LoginForm.tsx
+++ src/components/Forms/LoginForm/LoginForm.tsx
@@ -1,26 +1,43 @@
 import './LoginForm.css'
 
-import Form from 'react-bootstrap/Form'
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
 import { handleLoginSubmission } from '../../../handlers/handleLoginSubmission';
 import SubmitLoginButton from '../../Buttons/SubmitLoginButton/SubmitLoginButton';
 import RegisterNewUserButton from '../../Buttons/RegisterNewUserButton/RegisterNewUserButton';
+import { useNavigate } from 'react-router-dom';
 
-function LoginForm() {
+function LoginForm(props: { setShowLoadingModal: React.Dispatch<React.SetStateAction<boolean>>, setLoginErrorModal: React.Dispatch<React.SetStateAction<boolean>> }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const clearForm = () => {setUsername(''); setPassword('');}
+
+  const navigate = useNavigate()
+
   return (
     <div id="login-form">
-      <Form onSubmit={void handleLoginSubmission()}>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Control type="email" placeholder="Insira seu nome de usuário" />
+      <Form onSubmit={event => void handleLoginSubmission(event, username, password, clearForm, props.setShowLoadingModal, props.setLoginErrorModal, navigate)}>
+        <Form.Group className="mb-3" controlId="formBasicText">
+          <Form.Control
+            type="text"
+            placeholder="Insira seu nome de usuário"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Control type="password" placeholder="Insira sua senha" />
+          <Form.Control
+            type="password"
+            placeholder="Insira sua senha"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
         </Form.Group>
         <RegisterNewUserButton /> <SubmitLoginButton />
-        
-    </Form>
+      </Form>
     </div>
-    
-  )
+  );
 }
 
 export default LoginForm;
