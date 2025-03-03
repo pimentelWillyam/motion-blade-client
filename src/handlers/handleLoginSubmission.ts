b3ca4f3437fcc8e2b634665b2781463a254a7b32
Willyam Pimentel <willyampimenteldev@gmail.com>
2025-03-03 16:13:16 -0300

    feat: adding handle login submission

--- src/handlers/handleLoginSubmission.ts
+++ src/handlers/handleLoginSubmission.ts
@@ -1,5 +1,30 @@
-const handleLoginSubmission = () => {
-  console.log('login handled')
+import axios from "axios";
+
+const handleLoginSubmission = async  (event: React.FormEvent<HTMLFormElement>, userName: string, password: string, clearLoginForm: () => void,  setIsLoadingModal: React.Dispatch<React.SetStateAction<boolean>>, setLoginErrorModal: React.Dispatch<React.SetStateAction<boolean>>, navigate: NavigateFunction) => {
+  event.preventDefault();
+  const data = {
+    login: userName,
+    password: password,
+  };
+  try {
+    setIsLoadingModal(true)
+    clearLoginForm()
+    const response = await axios.post('http://localhost:4000/api/auth', data);
+    if (response.status === 200) {
+      localStorage.setItem('login', response.data.login);
+      localStorage.setItem('token', response.data.token);
+      navigate('/home')
+      setIsLoadingModal(false)
+    }
+    else {
+      setIsLoadingModal(false)
+      setLoginErrorModal(true)
+    }
+  }catch (error) {
+    console.error('Error during registration:', error);
+    setIsLoadingModal(false)
+    setLoginErrorModal(true)
+  }
 }
 
 export {handleLoginSubmission}
