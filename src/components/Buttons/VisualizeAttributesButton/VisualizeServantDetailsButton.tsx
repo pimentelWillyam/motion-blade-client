c3e33f68fb281e5d98b2d1bce4f4d8c39dfe51e5
Willyam Pimentel <willyampimenteldev@gmail.com>
2025-03-11 21:38:46 -0300

    feat: adding visualize servant details button

---
+++ src/components/Buttons/VisualizeAttributesButton/VisualizeServantDetailsButton.tsx
@@ -0,0 +1,13 @@
+import Button from 'react-bootstrap/Button'
+import './VisualizeServantDetailsButton.css'
+import { useNavigate } from 'react-router-dom'
+
+function VisualizeServantDetailsButton(props: {servantName: string}) {
+  const navigate = useNavigate()
+  return (
+    <Button onClick={() => navigate(`/servo/${props.servantName}`)} id = "back-to-login-page-button" variant="primary" type="submit"> Detalhes </Button>
+      
+  )
+}
+
+export default VisualizeServantDetailsButton
