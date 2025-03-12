b5e5668e88ee5f970d171c6990e29ab5e596254b
Willyam Pimentel <willyampimenteldev@gmail.com>
2025-03-11 21:39:41 -0300

    feat: adding my servants table

--- src/components/Tables/MyServantsTable/MyServantsTable.tsx
+++ src/components/Tables/MyServantsTable/MyServantsTable.tsx
@@ -1,33 +1,33 @@
 import { useEffect, useState } from 'react';
 import Table from 'react-bootstrap/Table';
-import VisualizeAttributesButton from '../../Buttons/VisualizeAttributesButton/VisualizeAttributesButton';
-import VisualizeInventoryButton from '../../Buttons/VisualizeInventoryButton/VisualizeInventoryButton';
+import VisualizeAttributesButton from '../../Buttons/VisualizeAttributesButton/VisualizeServantDetailsButton';
 import DatabaseServant from '../../../models/DatabaseServant';
 import axios from 'axios';
 import DeleteServantButton from '../../Buttons/DeleteServantButton/DeleteServantButton';
-import { handleLoginSubmission } from '../../../handlers/handleLoginSubmission';
 import { handleServantDeletion } from '../../../handlers/handleServantDeletion';
 
-function MyServantsTable() {
+function MyServantsTable(props: {setIsLoadingModal, setSuccessToast, setFailureToast}) {
   const [servants, setServants] = useState<DatabaseServant[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+  
+  const fetchServants = async () => {
+    try {
+      const response = await axios.get(`http://localhost:4000/api/servants/${localStorage.getItem('login')}`); 
+      if (response.status !== 200) {
+        throw new Error('Erro ao buscar os servos.');
+
+      }
+      setServants(response.data);
+    } catch (err) {
+      setError((err as Error).message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const fetchServants = async () => {
-      try {
-        const response = await axios.get(`http://localhost:4000/api/servants/${localStorage.getItem('login')}`); // Substitua pela URL real da API
-        if (response.status !== 200) {
-          throw new Error('Erro ao buscar os servos.');
-
-        }
-        setServants(response.data);
-      } catch (err) {
-        setError((err as Error).message);
-      } finally {
-        setLoading(false);
-      }
-    };
+    
 
     fetchServants();
   }, []);
@@ -50,9 +50,8 @@ function MyServantsTable() {
         {servants.map((servant) => (
           <tr key={servant.id}>
             <td>{servant.name}</td>
-            <td><VisualizeAttributesButton /></td>
-            <td><VisualizeInventoryButton /></td>
-            <td><DeleteServantButton onClick={() => handleServantDeletion(servant.name, props.setIsLoadingModal, props.setSuccessToast, setFailureToast)} /></td>
+            <td><VisualizeAttributesButton servantName={servant.name} /></td>
+            <td><DeleteServantButton onClick={() => handleServantDeletion(servant.name, props.setIsLoadingModal, fetchServants, props.setSuccessToast, props.setFailureToast)} /></td>
 
           </tr>
         ))}
